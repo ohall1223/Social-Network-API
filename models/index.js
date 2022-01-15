2695adcba5949b31798760ed216455d7ca1693ee
Olivia Hall <ohall1223@gmail.com>
2022-01-15 13:24:16 -0700

    models finished

---
+++ models/index.js
@@ -0,0 +1,7 @@
+// require users, thoughts & reaction Models
+const Thoughts = require('./Thought');
+const Users = require('./User')
+const Reactions = require('./Reaction') 
+
+// export 
+module.exports = { Thoughts, Users, Reactions }
